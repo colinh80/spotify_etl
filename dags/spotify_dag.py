b570f8e5bd489454cb134d4919646fbbd577bc39
Colin Hicks <80708120+colinjhicks@users.noreply.github.com>
2021-09-30 15:59:44 -0600

    remove email

--- dags/spotify_dag.py
+++ dags/spotify_dag.py
@@ -8,7 +8,7 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date' : datetime.now(),
-    'email': ['airflow@example.com'],
+    'email': ['@'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 0,
